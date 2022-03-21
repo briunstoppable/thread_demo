ab5b7ef52425477a2141829745d1c99b262dd70c
Brian Yaple <sup3rbr1an@gmail.com>
2022-03-21 03:51:10 +0100

    happy markdown update

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ g++ -std=c++11 -pthread thread_demo.cpp
 sha256sum thread_demo.cpp > hash.list
 sha256sum a.out >> hash.list
 
-echo " " >> README.MD
-echo recent build info: >> README.MD
+echo "# Brief"
+echo "A simple multithread application starting point."
+echo " " > README.MD
+echo "# Recent Build Info:" >> README.MD
 gcc -v >> README.MD
 date >> README.MD
 time >> README.MD
