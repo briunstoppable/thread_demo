ec35b71e07d6bbf76bfe5b032860686cce9b258e
Brian Yaple <sup3rbr1an@gmail.com>
2022-03-21 04:02:38 +0100

    Moved more stuff to markdown

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Brief
+Checking thread_demo.cpp ...
 A simple multithread application C++ starting point.
  
 # Recent Build Info:
 ## GCC Version
+gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
+Copyright (C) 2019 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+## Linux Test Build Info
+Linux xubuntu 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux
 ## Build Date
-Mon 21 Mar 2022 03:58:36 AM CET
+Mon 21 Mar 2022 04:02:38 AM CET
 ## Build Time
-## Hash List
+## SHA256 File Hash List
 8569d12624bac7da0fd8acdaaf0e86978115053640b3735926716730741bcf86  thread_demo.cpp
 4ef7e8ccbfa2dd96574ecebef04b0e6432b57d83761c3ec98ff281004ab6aa83  a.out
 ## Git Status
--- build.sh
+++ build.sh
@@ -1,27 +1,30 @@
 # Build the binary!
+echo "# Brief" > README.md
 
 echo Cleaning existing binary...
 rm a.out & \
 echo Running Linter... & \
-cppcheck --language=c++ --enable=warning,style --template=gcc "thread_demo.cpp" & \
+cppcheck --language=c++ --enable=warning,style --template=gcc "thread_demo.cpp" >> README.md & \
 echo Building... & \
-g++ -std=c++11 -pthread thread_demo.cpp
+g++ -std=c++11 -pthread thread_demo.cpp >> README.md
 sha256sum thread_demo.cpp > hash.list
 sha256sum a.out >> hash.list
 
 # Build the Readme!
 
-echo "# Brief" > README.md
+
 echo "A simple multithread application C++ starting point." >> README.md
 echo " " >> README.md
 echo "# Recent Build Info:" >> README.md
 echo "## GCC Version" >> README.md
-gcc -v >> README.md
+gcc --version >> README.md
+echo "## Linux Test Build Info" >> README.md
+uname -a >> README.md
 echo "## Build Date" >> README.md
 date >> README.md >> README.md
 echo "## Build Time" >> README.md
 time >> README.md >> README.md
-echo "## Hash List" >> README.md
+echo "## SHA256 File Hash List" >> README.md
 cat hash.list >> README.md
 echo "## Git Status" >> README.md
 git status >> README.md
