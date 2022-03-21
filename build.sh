1a64ac7eb6f739469dc403f470273f5844d357c6
Brian Yaple <sup3rbr1an@gmail.com>
2022-03-21 03:46:38 +0100

    a new automated build & commit tool. ::#;commito
    :s
    :q

--- build.sh
+++ build.sh
@@ -6,4 +6,15 @@ echo Building... & \
 g++ -std=c++11 -pthread thread_demo.cpp
 sha256sum thread_demo.cpp > hash.list
 sha256sum a.out >> hash.list
+
+echo " " >> readme.md 
+echo recent build info: >> readme.md
+gcc -v >> readme.md
+date >> readme.md
+time >> readme.md
+cat hash.list >> readme.md
+git status >> readme.md
+git add .
+git commit
+git push
 echo Complete.
